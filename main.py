b25bf902ec1e20b5c428ad38aa17efbede19727b
b991 <wangbeiqi2@gmail.com>
2023-08-13 01:39:56 -0700

    complete scraping, need to fix

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from bs4 import BeautifulSoup
+import requests
+import datetime
 
-print("Hello World")
+#def find_online_num():
+html_text = requests.get('https://allcp.net/').text
+soup = BeautifulSoup(html_text, 'lxml')
+count_div = soup.find('span',class_='xs1')
+count_text = count_div.find('strong').text
+now = datetime.datetime.now()
+timezone = datetime.datetime.now().astimezone().tzinfo
+print(count_text)
+print(now)
+print(timezone)
