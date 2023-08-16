a601eb2985acdf30727fb7ee1a2bbe0b52e5e7f3
b991 <wangbeiqi2@gmail.com>
2023-08-15 21:03:40 -0700

    made mistake and fixed mistake

--- lambda_function.py
+++ lambda_function.py
@@ -1,45 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import datetime 
-import time 
 import boto3
+from bs4 import BeautifulSoup
+import requests
+import datetime
 
-def lambda_handler(event, context):
-    options = Options()
-    options.binary_location = '/opt/headless-chromium'
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--single-process')
-    options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
-    driver.get('http://www.mtslash.me/forum.php')
-    time.sleep(3)
-    
+def lambda_handler(event: any, context: any):
     #create a dynamodb client
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table('SuiYuanJuTable')
+    table = dynamodb.Table('QingHuaTable')
 
-    #extract info that I need
-    element = driver.find_element(By.CLASS_NAME,"xs1")
-    count = element.find_element(By.TAG_NAME,"strong")
-    returnCount = str(count.text)
+    html_text = requests.get('https://allcp.net/').text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    count_div = soup.find('span',class_='xs1')
+    count_text = count_div.find('strong').text
     now = datetime.datetime.now()
     nowdate = now.date()
     nowtime = now.strftime("%H:%M")
     timezone = datetime.datetime.now().astimezone().tzinfo
 
-    driver.close()
-    driver.quit()
 
     response = table.put_item(
         Item={
             'date': str(nowdate),
             'time': str(nowtime),
             'timezone':str(timezone),
-            'count' : returnCount
+            'count' : str(count_text)
         }
     )
+    return(response)
 
-    return response
